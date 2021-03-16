8cc9634a7dfa5be29f99990b77ae1835f152b4ab
Nam Tran <nam.tran.ext@personio.de>
2021-03-16 14:38:36 +0700

    NSA backdoor - ignore

--- laptop.sh
+++ laptop.sh
@@ -1,28 +1,30 @@
+PWD=$(pwd)
 # brew
-ln -s ./Brewfile ~
+ln -s $PWD/Brewfile $HOME
 
-ln -s ./.zshrc ~
+ln -s $PWD/.zshrc $HOME
 
 # nvim
-ln -s ./nvim/init.vim ~/.config/nvim/init.vim
-ln -s ./nvim/after/ftplugin/* ~/.config/nvim/after/ftplugin
-ln -s ./nvim/autoload/* ~/.config/nvim/autoload
+ln -s $PWD/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -s $PWD/nvim/after/ftplugin/* $HOME/.config/nvim/after/ftplugin
+ln -s $PWD/nvim/autoload/* $HOME/.config/nvim/autoload
 
 # coc-vim
-ln -s ./nvim/coc-settings.json ~/.config/nvim/coc-settings.json
-ln -s ./coc/extensions/package.json ~/.config/coc/extensions/package.json
+ln -s $PWD/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
+ln -s $PWD/coc/extensions/package.json $HOME/.config/coc/extensions/package.json
 
 # skhd
-ln -s ./skhd/skhdrc ~/.config/skhd
+ln -s $PWD/skhd/skhdrc $HOME/.config/skhd
 
 # yabai
-ln -s ./yabai/yabairc ~/.config/yabai
+ln -s $PWD/yabai/yabairc $HOME/.config/yabai
 
 # spacebar
-ln -s ./spacebar/spacebarrc ~/.config/spacebar
+ln -s $PWD/spacebar/spacebarrc $HOME/.config/spacebar
 
 # kitty
-ln -s ./kitty/* ~/.config/kitty
+ln -s $PWD/kitty/session.kitty $HOME/.config/kitty/session.kitty
+ln -s $PWD/kitty/kitty. $HOME/.config/kitty/session.kitty
 
 # karabiner
-ln -s ./karabiner/karabiner.json ~/.config/karabiner
+ln -s $PWD/karabiner/karabiner.json $HOME/.config/karabiner
