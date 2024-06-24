e287933f6bc2563da23d29f642c12a8f716ee080
Nam Tran <nam.tran@employmenthero.com>
2024-06-24 11:53:26 +0700

    update laptop.sh

--- laptop.sh
+++ laptop.sh
@@ -1,40 +1,4 @@
 PWD=$(pwd)
-# brew
-ln -sf $PWD/Brewfile $HOME
-
-ln -sf $PWD/.zshrc $HOME
-ln -sf $PWD/.zsh_functions $HOME/.zsh_functions
-
-# asdf
-ln -sf $PWD/.tool-versions $HOME
-
-# tmux
-ln -sf $PWD/.tmux.conf $HOME
-
-# nvim
-# ln -s $PWD/nvim/init.vim $HOME/.config/nvim/init.vim
-# ln -s $PWD/nvim/after/ftplugin/* $HOME/.config/nvim/after/ftplugin
-# ln -s $PWD/nvim/autoload/* $HOME/.config/nvim/autoload
-
-# coc-vim
-# ln -s $PWD/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
-# ln -s $PWD/coc/extensions/package.json $HOME/.config/coc/extensions/package.json
-
-# skhd
-# ln -s $PWD/skhd/skhdrc $HOME/.config/skhd
-
-# yabai
-# ln -s $PWD/yabai/yabairc $HOME/.config/yabai
-
-# spacebar
-# ln -s $PWD/spacebar/spacebarrc $HOME/.config/spacebar
-
-# kitty
-# ln -s $PWD/kitty/session.kitty $HOME/.config/kitty/session.kitty
-# ln -s $PWD/kitty/session.conf $HOME/.config/kitty/session.conf
 
 # karabiner
-# ln -s $PWD/karabiner/karabiner.json $HOME/.config/karabiner
-
-# tmux
-# ln -s $PWD/.tmux.conf $HOME/.tmux.conf
+ln $HOME/.config/karabiner/karabiner.json $PWD/karabiner
