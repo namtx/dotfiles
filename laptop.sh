765debdbc7f10ad83d187715f2fa6791f00ddd71
Nam Tran <nam.tran@employmenthero.com>
2024-07-11 08:31:27 +0700

    zsh_fuctions

--- laptop.sh
+++ laptop.sh
@@ -2,3 +2,6 @@ PWD=$(pwd)
 
 # karabiner
 ln $HOME/.config/karabiner/karabiner.json $PWD/karabiner
+
+# zsh functions
+ln $HOME/.zsh_functions $PWD/zsh/
---
+++ laptop_backup.sh
@@ -0,0 +1,3 @@
+PWD=$(pwd)
+
+cp $HOME/.zsh_functions $PWD/zsh/
